52cdde388eab00e1f3893d1408a37345cc12c6a7
vinayak jadhav <vinayak.jadhav333@live.com>
2024-09-04 00:42:43 +0100

    Module update

--- dist/index.js
+++ dist/index.js
@@ -31197,14 +31197,17 @@ async function run() {
         }
         let count = 0;
         const tokenizedFiles = getFiles(filesPath);
+        console.log(`\nFound below files at ${filesPath}`);
         console.log(tokenizedFiles);
         const envVariables = process.env;
         for (const tokenizedFile of tokenizedFiles) {
             console.log(`\nChecking and replacing tokens in ${tokenizedFile}`);
             let rawContent = fs.readFileSync(`${tokenizedFile}`, 'utf-8');
+            console.log(rawContent);
             // Loop through each environment variable
             for (const [key, value] of Object.entries(envVariables)) {
                 const matchValue = `${tokenPrefix}${key}${tokenSuffix}`;
+                console.log(matchValue);
                 if (rawContent.includes(matchValue)) {
                     rawContent = rawContent.replace(new RegExp(matchValue, 'g'), value);
                     console.log(`${key} value updated in ${tokenizedFile}`);
@@ -31214,6 +31217,7 @@ async function run() {
             // Loop through additional variables
             for (const variable of variables) {
                 const matchValue = `${tokenPrefix}${variable.name}${tokenSuffix}`;
+                console.log(matchValue);
                 if (rawContent.includes(matchValue)) {
                     rawContent = rawContent.replace(new RegExp(matchValue, 'g'), variable.value);
                     console.log(`${variable.name} value updated in ${tokenizedFile}`);
