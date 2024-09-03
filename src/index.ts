ca9851be132999cfd20c4a25b8c1971852ce13bf
vinayak jadhav <vinayak.jadhav333@live.com>
2024-09-03 11:02:29 +0100

    Updated action for remote repo

--- src/index.ts
+++ src/index.ts
@@ -51,7 +51,7 @@ export async function run() {
                 const variableValue = variable.value;
                 var command = '"echo "' + variableName + "=" + variableValue + '" >> $GITHUB_ENV"';
                 console.log(command);
-                exec("set ${variableName}=${variableValue}", (error, stdout, stderr) => {
+                exec('set ${variableName}=${variableValue}', (error, stdout, stderr) => {
                     if (error) {
                         console.log(`error: ${error.message}`);
                         return;
@@ -62,7 +62,7 @@ export async function run() {
                     }
                     console.log(`stdout: ${stdout}`);
                 });   
-                exec("set bbbhhs=44545", (error, stdout, stderr) => {
+                exec('set bbbhhs=44545', (error, stdout, stderr) => {
                     if (error) {
                         console.log(`error: ${error.message}`);
                         return;
