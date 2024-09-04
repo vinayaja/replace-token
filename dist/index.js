d674f6241ef17d3bbbb1940be4cc3e90ad966df9
vinayak jadhav <vinayak.jadhav333@live.com>
2024-09-04 01:26:40 +0100

    Module update

--- dist/index.js
+++ dist/index.js
@@ -31209,7 +31209,7 @@ async function run() {
                 const matchValue = `${tokenPrefix}${key}${tokenSuffix}`;
                 console.log(matchValue);
                 if (rawContent.includes(matchValue)) {
-                    rawContent = rawContent.replace(new RegExp(matchValue, 'i,g'), value);
+                    rawContent = rawContent.replace(new RegExp(matchValue, 'g'), value);
                     console.log(`${key} value updated in ${tokenizedFile}`);
                     count++;
                 }
@@ -31219,7 +31219,7 @@ async function run() {
                 const matchValue = `${tokenPrefix}${variable.name}${tokenSuffix}`;
                 console.log(matchValue);
                 if (rawContent.includes(matchValue)) {
-                    rawContent = rawContent.replace(new RegExp(matchValue, 'i,g'), variable.value);
+                    rawContent = rawContent.replace(new RegExp(matchValue, 'g'), variable.value);
                     console.log(`${variable.name} value updated in ${tokenizedFile}`);
                     count++;
                 }
